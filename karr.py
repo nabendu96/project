433f8af6a9bad9835866e0a26776235f13e5416f
nabendu96 <62113982+nabendu96@users.noreply.github.com>
2021-01-11 04:23:14 +0530

    Update karr.py

--- karr.py
+++ karr.py
@@ -11,22 +11,6 @@ import numpy as np
 import sys
 
 def get_velscale(filename, n_pixels, n_los):
-    #norm = 0.9179910109957352        # fiducial 
-    #norm = 0.8957986327622899        # non-power law
-    #norm = 0.4931288366537928        # 10*T_0
-    #norm = 0.7835412796289518        # 2*T_0
-    #norm = 0.6490402406402864        # 4*T_0
-    #norm = 0.5760575572902173        # 6*T_0
-    #norm = 0.5280126690283625        # 8*T_0
-    #norm = 0.6436081830025281        # 2*slope
-    #norm = 0.875788437770983         # 1.2*slope
-    #norm = 0.8266538422620232        # 1.4*slope
-    #norm = 0.770737346393422         # 1.6*slope
-    #norm = 0.7089911633630724        # 1.8*slope
-    #norm = 0.9174169704210932        # -ve power at low temperature
-    #norm = 0.4924037489101563        # 10*T_0 and -ve power at low temperature
-    #norm = 0.9286380805401929        # fiducial with 512 grids
-    #norm = 0.524817614661789         # 10*T_0 with 512 grids
     with open(filename, 'rb') as f:
         for i in range(n_los):
             n = np.fromfile(f, dtype=np.int64, count=1)
